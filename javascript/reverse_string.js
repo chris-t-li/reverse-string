85b5a06c4d09e0d7abd9d21c08deff433f0f5f4e
Chris Li <chris.lty07@gmail.com>
2022-10-05 09:39:45 -0400

    Reverse a String solution

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,16 @@
+// Use Array Methods
+function reverseString1(str) {
+  return str.split("").reverse().join("");
+}
+
+// Use iteraction
 function reverseString(str) {
-  // type your code here
+  // convert string param into an array
+  const strArray = str.split("");
+  // get length of array 
+  const n = strArray.length-1;
+  const reverseArray = strArray.map((element, index, array) => element = array[n - index])
+  return reverseArray.join("");  
 }
 
 if (require.main === module) {
